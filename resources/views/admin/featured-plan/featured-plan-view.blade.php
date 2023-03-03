c2372798964cf3ea0fde6acf148551893886a2d8
DharaKhandhediya2025 <dhara.theappideas@gmail.com>
2023-03-03 12:38:12 +0530

    Featured Plan view page

--- resources/views/admin/featured-plan/featured-plan-view.blade.php
+++ resources/views/admin/featured-plan/featured-plan-view.blade.php
@@ -27,7 +27,48 @@
         </div>
         <div class="col s12">
             <div class="container">
-                
+                 <div class="card">
+                        <div class="card-content">
+                            <div class="row">
+                                <div class="col s12 m12">
+                                    <table class="table striped">
+                                        <tbody>
+                                            <tr>
+                                                <b><th>Plan Type :</th></b>
+                                                <td>{{ $featured_plan->type }}</td>
+
+                                                <b><th>Price :</th></b>
+                                                <td>{{ $featured_plan->price }}</td>
+
+                                                <b><th>Validity :</th></b>
+                                                <td>{{ $featured_plan->validity }} {{ $featured_plan->duration }}</</td>
+                                            </tr>
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                @if(isset($featured_plan->plan_functionality))
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="row">
+                                <div class="col s12 m12">
+                                    <table class="table striped">
+                                        <tbody>
+                                                <b><th>Functionalities :</th></b>
+                                                @foreach($featured_plan->plan_functionality as $functionality)
+                                                   <tr style="margin-left: 20px;"><td><li>{{ $functionality->functionality }}</li></td></tr>
+                                                @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
